0ccc9c6b5dad473517a5dc6a2797c93062e96b4d
Thiksha <35593877+Thiksha@users.noreply.github.com>
2018-09-12 12:33:36 +0530

    Add files via upload

---
+++ thikshab.java
@@ -0,0 +1,14 @@
+class thikshab {
+   public static void main(String[] args) {    	
+      int number = -8;
+	 
+      if (number > 0) {
+         System.out.println("Number is positive.");
+      }
+      else {
+         System.out.println("Number is not positive.");
+      }
+ 
+ 
+   }
+}
---
+++ thikshac.java
@@ -0,0 +1,64 @@
+class thikshac {
+	public static void main(String[] args) {
+
+int year=10;
+String month;
+
+switch(year) {
+
+	case 1:
+	 month = "jan";
+	 break;
+	
+	case 2:
+	 month = "feb";
+	 break;
+	
+	case 3:
+	 month = "mar";
+	 break;
+
+	case 4:
+	 month = "apr";
+	 break;
+
+	case 5:
+	 month = "may";
+	 break;
+
+	case 6:
+	 month = "jun";
+	 break;
+
+	case 7:
+	 month = "jul";
+	 break;
+
+	case 8:
+	 month = "aug";
+	 break;
+
+	case 9:
+	 month = "sep";
+	 break;
+
+	case 10:
+	 month = "oct";
+	 break;
+
+	case 11:
+	 month = "nov";
+	 break;
+
+	case 12:
+	 month = "dec";
+	 break;
+
+	default:
+	 month = "invalid month";
+	 break;
+}
+System.out.println(month);
+  }
+ }
+
