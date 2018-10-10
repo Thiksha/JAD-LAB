e76a99e9896c06e58894d34a75576dc5f576759d
Thiksha <35593877+Thiksha@users.noreply.github.com>
2018-10-10 13:00:50 +0530

    Update strings.java

--- strings.java
+++ strings.java
@@ -1,9 +1,8 @@
-import java.util.*;
+public class StringDemo {
 
-public class string1{
-   public static void main(String[] args){
-      double e = Math.E;
-      System.out.format("%f%n", e);
-      System.out.format(Locale.GERMANY, "%-10.4f%n%n", e);
+   public static void main(String args[]) {
+      char[] helloArray = { 'h', 'e', 'l', 'l', 'o', '.' };
+      String helloString = new String(helloArray);  
+      System.out.println( helloString );
    }
 }
